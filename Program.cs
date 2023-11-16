41638cad2c189a1994ab442a3d31760ecf27f336
Ivan21T <ivan_tashev_gmail.bg@abv.bg>
2023-11-16 16:13:37 +0200

    The result of sum

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Сума_отрицателни_нечетни
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] separator = { ',' };
+            List<int> nums=Console.ReadLine().Split(separator,StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            List<int> oddNums=new List<int>();
+            for (int i = 0; i < nums.Count-1; i++)
+            {
+                if (nums[i] < 0 && nums[i] % 2 != 0)
+                {
+                    if (i >= 0 && nums[i + 1] < 0 && nums[i + 1] % 2 != 0)
+                    {
+                        oddNums.Add(nums[i]);
+                    }
+                    else if (i != 0 && nums[i - 1] < 0 && nums[i - 1] % 2 != 0 && (nums[i + 1] >= 0 || nums[i + 1] % 2 == 0))
+                    {
+                        oddNums.Add(nums[i]);
+                    }
+                    else if (i != 0 && nums[i + 1] < 0 && nums[i + 1] % 2 != 0 && (nums[i - 1] >= 0 || nums[i - 1] % 2 == 0))
+                    {
+                        oddNums.Add(nums[i]);
+                    }
+                }    
+            }
+            if (nums.Count!=1 && nums[nums.Count-1]<0 && nums[nums.Count-1]%2!=0 && nums[nums.Count-2]<0 && nums[nums.Count-2]%2!=0)
+            {
+                oddNums.Add(nums[nums.Count - 1]);
+            }
+            Console.WriteLine("{0}",string.Join(",",oddNums));
+            Console.WriteLine("Sum:{0}",oddNums.Sum());
+        }
+    }
+}
